50b6e64250c4fa449c285e33d0934e7f61619539
Purple Myst <PurpleMyst@users.noreply.github.com>
2018-06-10 17:21:04 +0200

    Fix MathJax URL query string

--- js/index.js
+++ js/index.js
@@ -17,7 +17,7 @@
     // NB: MathJax doesn't really.. do anything with RequireJS. You can run it
     // under RequireJs, but it still just defines its stuff under
     // `window.MathJax`, not with a `requirejs.define`.  ¯\_(ツ)_/¯
-    const MATHJAX_URL = "https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.1/MathJax.js";
+    const MATHJAX_URL = "https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.1/MathJax.js?config=AM_HTMLorMML&delayStartupUntil=configured";
     require([MATHJAX_URL, "./js/lz-string.min.js", "ace/ace"], function(_, LZString, ace) {
         const $renderedLines = document.getElementById(RENDERED_LINES_ID);
         let lineElements = [];
