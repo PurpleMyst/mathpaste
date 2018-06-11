b922070487ae503b2208244788e4f7bf6e78a1a5
Purple Myst <PurpleMyst@users.noreply.github.com>
2018-06-11 18:21:23 +0200

    Implement the backend

--- index.js
+++ index.js
@@ -3,14 +3,55 @@
 
 const redis = require("redis");
 const express = require("express");
+const crypto = require("crypto");
+const bodyParser = require("body-parser");
 
 const client = redis.createClient();
 const app = express();
 
-client.on("error", err => console.error(err));
+client.on("error", err => console.error(`Error: ${err}`));
 
+app.use(bodyParser.text({ "type": "*/*" }));
 app.use("/mathpaste", express.static("mathpaste"));
 
-// TODO: A redis backend.
+app.get("/mathpaste/api/:pasteId", (req, res) => {
+  console.log("GET");
+  const pasteId = req.params.pasteId;
 
-app.listen(process.env.PORT || 3000);
+  client.get(`mathpaste:${pasteId}`, (err, reply) => {
+    if (err !== null) {
+      res.status(400);
+      res.send({"ok": false, "error": err});
+      return;
+    }
+
+    if (reply === null) {
+      res.status(404);
+      res.send({"ok": false, "error": "No such key."});
+    } else {
+      res.send({"ok": true, "contents": reply});
+    }
+  });
+});
+
+app.post("/mathpaste/api", (req, res) => {
+  if (JSON.stringify(req.body) === JSON.stringify({})) {
+    res.status(400);
+    res.send({"ok": false, "error": "Invalid request body or content type."});
+    return;
+  }
+
+  const pasteId = crypto.createHash("sha256").update(req.body).digest("hex");
+
+  client.set(`mathpaste:${pasteId}`, req.body, (err, reply) => {
+    if (err !== null) {
+      res.status(400);
+      res.send({"ok": false, "error": err});
+      return;
+    }
+
+    res.send({"ok": true, "id": pasteId});
+  });
+});
+
+app.listen(process.env.PORT || 8080);
